e40c2939ab87e8b5a0f1294f6237688bb515e1e9
YawoSadji <yawo.sadji@gmx.com>
2024-09-16 15:06:38 -0400

    yawo modification1

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
-
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=uk5GEpqnpQCO2LtVRyw67eoB3cjqTMWC&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+async function getImage(query){
+    try{
+        const response = await fetch(endpoint);
+        const data = await response.json();
+        const imgUrl = data.url;
+        console.log(imgUrl);
+
+    }catch(err){
+        console.error(err)
+    }
+}
