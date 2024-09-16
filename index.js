efd94f50fbe52c0d3052beef9a20fb7baf6afb21
bjjones8512 <bjjones8512@yahoo.com>
2024-09-16 12:06:41 -0500

    API setup

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ require('dotenv').config();
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=uk5GEpqnpQCO2LtVRyw67eoB3cjqTMWC&q=Week5&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
