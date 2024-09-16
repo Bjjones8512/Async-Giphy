a2742d0ea5e7684fe60bb11b6807a76d03bffa99
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-09-16 16:02:57 -0400

    Random vaiable

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // Print out value of API key stored in .env file
 // console.log(process.env.API_KEY)
 
-const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=uk5GEpqnpQCO2LtVRyw67eoB3cjqTMWC&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips"
+const endpoint =
+  "https://api.giphy.com/v1/gifs/search?api_key=uk5GEpqnpQCO2LtVRyw67eoB3cjqTMWC&q=dogs&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
-async function getImage(query){
-    try{
-        const response = await fetch(endpoint);
-        const data = await response.json();
-        const imgUrl = data.data[0].images.original.url;
-        console.log(imgUrl);
+async function getImage(query) {
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const random = Math.floor(Math.random() * 25);
+    const imgUrl = data.data[random].images.original.url;
 
-        return imgUrl
-    }catch(err){
-        console.error(err)
-    }
+    console.log(imgUrl);
+    console.log(random);
+
+    return imgUrl;
+  } catch (err) {
+    console.error(err);
+  }
 }
-getImage()
+getImage();
